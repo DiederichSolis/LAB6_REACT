ec87abb22df32d3c708787d8c27367491bdedbfc
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-26 12:12:05 -0600

    feat: finish

--- futbol/futbol.js
+++ futbol/futbol.js
@@ -4,15 +4,15 @@ const NavBar = () => {
     const styles = {
         position: 'fixed',
         top: 0,
-        background: 'rgba(0, 0, 0, 0.5)',
-        backdropFilter: 'blur(10px)',
-        color: 'white',
+        zIndex: 1000,
+        background: 'linear-gradient(to bottom, rgba(10, 10, 10, 1), rgba(10, 10, 10, 0))',
+        color: 'black',
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
-        height: '10vh',
+        height: '15vh',
         width: '100%',
-        zIndex: 1000,
+        padding: '0 5%',
     };
 
     const logoContainer = {
@@ -27,19 +27,21 @@ const NavBar = () => {
         width: '7%',
     };
 
+    const navLinks = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+        margin: '0 20%',
+        gap: '30%', // Espacio entre los enlaces
+    };
+
     const iconStyles = {
         color: 'white',
         marginLeft: '10px',
         marginRight: '10px',
         textDecoration: 'none',
         fontSize: '1.5em',
-    };
-
-    const navLinks = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexGrow: 1,
     };
 
     const titulos = {
@@ -52,25 +54,30 @@ const NavBar = () => {
     };
 
     return (
-        <nav style={styles}>
-            <div style={logoContainer}>
-                <img src='../multimedia/champions.png' alt="logo" width="40" height="40" style={logo} />
-                <a href="https://www.instagram.com/championsleague" style={iconStyles}>
-                    <i className="fab fa-instagram"></i>
-                </a>
-                <a href="https://www.facebook.com/uefachampionsleague" style={iconStyles}>
-                    <i className="fab fa-facebook"></i>
-                </a>
-                <a href="https://www.youtube.com/channel/UCpcTrCXblq78GZrTUTLWeBw" style={iconStyles}>
-                    <i className="fab fa-youtube"></i>
-                </a>
-            </div>
-            <div style={navLinks}>
-                <a href='../index.html' style={titulos}>HOME</a>
-                <a href='../champions/champions.html' style={titulos}>CHAMPIONS</a>
-                <a href='../partidos/partidos.html' style={titulos}>SEMIFINAL</a>
-            </div>
-        </nav>
+
+         <nav style={styles}>
+
+<div style={logoContainer}>
+   
+    <a href="https://www.instagram.com/championsleague" style={iconStyles}>
+        <i className="fab fa-instagram"></i>
+    </a>
+    <a href="https://www.facebook.com/uefachampionsleague" style={iconStyles}>
+        <i className="fab fa-facebook"></i>
+    </a>
+    <a href="https://www.youtube.com/channel/UCpcTrCXblq78GZrTUTLWeBw" style={iconStyles}>
+        <i className="fab fa-youtube"></i>
+    </a>
+</div>
+
+<div style={navLinks}>
+
+    <a href='index.html' style={titulos}>HOME</a>
+    <a href='champions/champions.html' style={titulos}>CHAMPIONS</a>
+    <a href='partidos/partidos.html' style={titulos}>PARTIDOS</a>
+    <a href='futbol/futbol.html' style={titulos}>FUTBOL</a>
+</div>
+</nav>
     );
 };
 
@@ -149,7 +156,10 @@ const Standings = () => {
 
     return (
         <div style={cardStyles}>
-             <h1 style={titleStyles}>tables</h1>
+             <h1 style={titleStyles}>⚽</h1>
+             <h1 style={titleStyles}>-</h1>
+             <h1 style={titleStyles}>-</h1>
+             <h1 style={titleStyles}>-</h1>
             <h1 style={titleStyles}>Champions League</h1>
             <h1 style={titleStyles}>Season 2023</h1>
             {standings.length > 0 ? (
